b6c60d5d0859faeec69f46fca958b59232d994d6
huzaifaghazali <huzaifaghazali@gmail.com>
2024-02-13 20:48:44 +0500

    Fix typo

--- backend/models/ExpenseModel.js
+++ backend/models/ExpenseModel.js
@@ -18,7 +18,7 @@ const ExpenseSchema = new mongoose.Schema(
       type: String,
       default: 'expense',
     },
-    data: {
+    date: {
       type: Date,
       required: true,
       trim: true,
