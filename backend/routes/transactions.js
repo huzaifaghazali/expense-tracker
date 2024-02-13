9d6b7f4d20e980562e4b2ceb222863b58ec63ac1
huzaifaghazali <huzaifaghazali@gmail.com>
2024-02-13 21:18:18 +0500

    Create addIncome function in income controller

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,9 +1,7 @@
+const { addIncome } = require('../controllers/income');
 
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-   res.send('Hello world');
-   
-})
+router.post('/add-income', addIncome);
 
 module.exports = router;
