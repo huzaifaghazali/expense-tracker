3f6ea1aa3b8fa16779403eeb71fee9ce28a70b2b
huzaifaghazali <huzaifaghazali@gmail.com>
2024-02-13 20:14:16 +0500

    Connected with mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { db } = require('./db/connect');
+const app = express();
 
 const PORT = process.env.PORT;
 
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+
 const server = () => {
+   db();
+   app.listen(PORT, () => {
    console.log(`You are listening to port: ${PORT}`);
+   })
 }
 
 server();
