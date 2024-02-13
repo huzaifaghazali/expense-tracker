5e8ae3aa6ecd59a4c0c38ece788fbbb5b77cd4fb
huzaifaghazali <huzaifaghazali@gmail.com>
2024-02-13 20:38:15 +0500

    Add route middlware

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { db } = require('./db/connect');
+const { readdirSync } = require('fs');
 const app = express();
 
 const PORT = process.env.PORT;
@@ -10,12 +11,16 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors());
 
+// Routes
+readdirSync('./routes').map((route) =>
+  app.use('/api/v1', require('./routes/' + route))
+);
 
 const server = () => {
-   db();
-   app.listen(PORT, () => {
-   console.log(`You are listening to port: ${PORT}`);
-   })
-}
+  db();
+  app.listen(PORT, () => {
+    console.log(`You are listening to port: ${PORT}`);
+  });
+};
 
 server();
