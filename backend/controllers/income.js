4541a488bcf1862a75353d37a922402b379bb28b
huzaifaghazali <huzaifaghazali@gmail.com>
2024-02-13 21:40:14 +0500

    Add deleteIncome funciton in income controller

--- backend/controllers/income.js
+++ backend/controllers/income.js
@@ -33,7 +33,21 @@ const getIncome = async (req, res) => {
   }
 };
 
+const deleteIncome = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const income = await IncomeSchema.findByIdAndDelete(id);
+    if (!income) {
+      return res.status(404).json({ message: 'Income not found' });
+    }
+    res.status(200).json({ message: 'Income Deleted' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
 module.exports = {
   addIncome,
   getIncome,
+  deleteIncome
 };
--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,7 +1,10 @@
-const { addIncome, getIncome } = require('../controllers/income');
+const { addIncome, getIncome, deleteIncome } = require('../controllers/income');
 
 const router = require('express').Router();
 
-router.post('/add-income', addIncome).get('/get-incomes', getIncome);
+router
+  .post('/add-income', addIncome)
+  .get('/get-incomes', getIncome)
+  .delete('/delete-income/:id', deleteIncome);
 
 module.exports = router;
