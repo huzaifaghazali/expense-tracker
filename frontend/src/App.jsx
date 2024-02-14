760f4824b225cb66bca4fc2db64e3345dd452752
huzaifaghazali <huzaifaghazali@gmail.com>
2024-02-14 19:26:24 +0500

    Style the app component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,33 @@
-
+import styled from 'styled-components';
+import { MainLayout } from './styles/Layout';
 
 function App() {
- 
-
   return (
-    <>
-      Hello everybody
-    </>
-  )
+    <AppStyled>
+      <MainLayout>
+         
+        <main>
+        </main>
+      </MainLayout>
+    </AppStyled>
+  );
 }
 
-export default App
+const AppStyled = styled.div`
+  height: 100vh;
+  background-image: url('./bg.png');
+  position: relative;
+  main {
+    flex: 1;
+    background: rgba(252, 246, 249, 0.78);
+    border: 3px solid #ffffff;
+    backdrop-filter: blur(4.5px);
+    border-radius: 32px;
+    overflow-x: hidden;
+    &::-webkit-scrollbar {
+      width: 0;
+    }
+  }
+`;
+
+export default App;
