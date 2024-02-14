554563f71c96da789a84237a0f9dbce415f6c0ee
huzaifaghazali <huzaifaghazali@gmail.com>
2024-02-14 20:01:21 +0500

    Add custom useWindowSize hook

--- frontend/src/Components/Orb.jsx
+++ frontend/src/Components/Orb.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
+import { useWindowSize } from '../hooks/useWindowSize';
 
 function Orb() {
+  const { width, height } = useWindowSize();
+
   const moveOrb = keyframes`
    0%{
        transform: translate(0, 0);
    }
    50%{
-       transform: translate(400px, 500px);
+      transform: translate(${width}px, ${height / 2}px);
    }
    100%{
        transform: translate(0, 0);
