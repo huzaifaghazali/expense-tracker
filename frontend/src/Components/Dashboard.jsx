29f8e2809e67b7a098156980d18b487515f60d90
huzaifaghazali <huzaifaghazali@gmail.com>
2024-02-26 21:15:01 +0500

    Update dashboard component

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 import { InnerLayout } from '../styles/Layout';
 import { dollar } from '../utils/Icons';
 import Chart from './Chart';
+import { useGlobalContext } from '../context/globalContext';
 
 function Dashboard() {
+  const {
+    totalExpenses,
+    incomes,
+    expenses,
+    totalIncome,
+    totalBalance,
+    getIncomes,
+    getExpenses,
+  } = useGlobalContext();
+
+  useEffect(() => {
+    getIncomes();
+    getExpenses();
+  }, []);
+
   return (
     <DashboardStyled>
       <InnerLayout>
@@ -16,15 +32,21 @@ function Dashboard() {
             <div className='amount-con'>
               <div className='income'>
                 <h2>Total Income</h2>
-                <p>{dollar} 1234</p>
+                <p>
+                  {dollar} {totalIncome()}
+                </p>
               </div>
               <div className='expense'>
                 <h2>Total Expense</h2>
-                <p>{dollar} 1234</p>
+                <p>
+                  {dollar} {totalExpenses()}
+                </p>
               </div>
               <div className='balance'>
                 <h2>Total Balance</h2>
-                <p>{dollar} 1234</p>
+                <p>
+                  {dollar} {totalBalance()}
+                </p>
               </div>
             </div>
           </div>
@@ -34,15 +56,15 @@ function Dashboard() {
               Min <span>Salary</span>Max
             </h2>
             <div className='salary-item'>
-              <p>1234</p>
-              <p>1234</p>
+              <p>${Math.min(...incomes.map((item) => item.amount))}</p>
+              <p>${Math.max(...incomes.map((item) => item.amount))}</p>
             </div>
             <h2 className='salary-title'>
               Min <span>Expense</span>Max
             </h2>
             <div className='salary-item'>
-              <p>1234</p>
-              <p>1234</p>
+              <p>${Math.min(...expenses.map((item) => item.amount))}</p>
+              <p>${Math.max(...expenses.map((item) => item.amount))}</p>
             </div>
           </div>
         </div>
